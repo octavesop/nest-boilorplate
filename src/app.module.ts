87e5b6c2784608e62acf147c6a76dd06dc492c62
octavesop <octavesop@gmail.com>
2023-08-09 02:41:49 +0900

    :necktie: Pipe 및 guard 전역 설정 선언

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -21,6 +22,16 @@ import { AppService } from './app.service';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: JwtAuthGuard, // 전역 적용의 경우 주석 해제
+    // },
+    AppService,
+  ],
 })
 export class AppModule {}
